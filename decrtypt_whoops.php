7374bd92520b606b22ad71f4079236a67f9189ff
robinslager <43774555+robinslager@users.noreply.github.com>
2018-10-03 15:25:10 +0200

    Add files via upload

--- decrtypt_whoops.php
+++ decrtypt_whoops.php
@@ -1,243 +1,44 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Marti
+ * Date: 10/3/2018
+ * Time: 12:51 PM
+ */
 
-$char_1 = 'a';
-$char_2 = 'a';
-$char_3 = 'a';
-$char_4 = 'a';
-$pass = $char_1 . $char_2 . $char_3 . $char_4;
-goto start;
-
-
-letters:
-
-
-//// let the first one go up 1  until z than change it to uppercase when it hits Z make number of it (0)
-//// if number is equal to 9 let make char_2 go 1 up enz..
-if (substr($pass, -4, 4) === '9999'){
-    goto check;
-}
-elseif (substr($pass, -4, 3) === '999'){
-    $char_1 = 'a';
-    $char_2 = 'a';
-    $char_3 = 'a';
-    ++$char_4;
-    goto check;
-}
-elseif (substr($pass, -4, 2) === '99'){
-    $char_1 = 'a';
-    $char_2 = 'a';
-    ++$char_3;
-    goto check;
-}
-elseif (substr($pass, -4, 1) === '9'){
-    $char_1 = 'a';
-    ++$char_2;
-    goto check;
-}
-if($char_1 === 'z'){
-    $char_1 = 'A';
-
-    goto check;
-
-}
-elseif($char_1 === 'Z'){ $char_1 = '!'; goto check; }
-elseif($char_1 === '!'){ $char_1 = '~'; goto check; }
-elseif($char_1 === '~'){ $char_1 = '@'; goto check; }
-elseif($char_1 === '@'){ $char_1 = '#'; goto check; }
-elseif($char_1 === '#'){ $char_1 = '$'; goto check; }
-elseif($char_1 === '$'){ $char_1 = '%'; goto check; }
-elseif($char_1 === '%'){ $char_1 = '^'; goto check; }
-elseif($char_1 === '^'){ $char_1 = '&'; goto check; }
-elseif($char_1 === '&'){ $char_1 = '*'; goto check; }
-elseif($char_1 === '*'){ $char_1 = '('; goto check; }
-elseif($char_1 === '('){ $char_1 = ')'; goto check; }
-elseif($char_1 === ')'){ $char_1 = '_'; goto check; }
-elseif($char_1 === '_'){ $char_1 = '-'; goto check; }
-elseif($char_1 === '-'){ $char_1 = '+'; goto check; }
-elseif($char_1 === '+'){ $char_1 = '='; goto check; }
-elseif($char_1 === '='){ $char_1 = '{'; goto check; }
-elseif($char_1 === '{'){ $char_1 = '}'; goto check; }
-elseif($char_1 === '}'){ $char_1 = '['; goto check; }
-elseif($char_1 === '['){ $char_1 = ']'; goto check; }
-elseif($char_1 === ']'){ $char_1 = '|'; goto check; }
-elseif($char_1 === '|'){ $char_1 = ';'; goto check; }
-elseif($char_1 === ';'){ $char_1 = ':'; goto check; }
-elseif($char_1 === ':'){ $char_1 = '<'; goto check; }
-elseif($char_1 === '<'){ $char_1 = '>'; goto check; }
-elseif($char_1 === '>'){ $char_1 = ','; goto check; }
-elseif($char_1 === ','){ $char_1 = '.'; goto check; }
-elseif($char_1 === '.'){ $char_1 = '/'; goto check; }
-elseif($char_1 === '/'){ $char_1 = '?'; goto check; }
-elseif($char_1 === '?'){ $char_1 = '`'; goto check; }
-elseif($char_1 === '`'){ $char_1 = '£'; goto check; }
-elseif($char_1 === '£'){ $char_1 = '€'; goto check; }
-elseif($char_1 === '€'){ $char_1 = '¢'; goto check; }
-elseif($char_1 === '¢'){ $char_1 = '¥'; goto check; }
-elseif($char_1 === '¥'){ $char_1 = '§'; goto check; }
-elseif($char_1 === '§'){ $char_1 = '0'; goto check; }
-if($char_2 === 'z' && $char_1 === 'a'){
-    $char_2 = 'A';
-
-    goto check;
-}
-
-elseif($char_2 === 'Z' && $char_1 === 'a'){ $char_2 = '!'; goto check; }
-elseif($char_2 === '!' && $char_1 === 'a'){ $char_2 = '~'; goto check; }
-elseif($char_2 === '~' && $char_1 === 'a'){ $char_2 = '@'; goto check; }
-elseif($char_2 === '@' && $char_1 === 'a'){ $char_2 = '#'; goto check; }
-elseif($char_2 === '#' && $char_1 === 'a'){ $char_2 = '$'; goto check; }
-elseif($char_2 === '$' && $char_1 === 'a'){ $char_2 = '%'; goto check; }
-elseif($char_2 === '%' && $char_1 === 'a'){ $char_2 = '^'; goto check; }
-elseif($char_2 === '^' && $char_1 === 'a'){ $char_2 = '&'; goto check; }
-elseif($char_2 === '&' && $char_1 === 'a'){ $char_2 = '*'; goto check; }
-elseif($char_2 === '*' && $char_1 === 'a'){ $char_2 = '('; goto check; }
-elseif($char_2 === '(' && $char_1 === 'a'){ $char_2 = ')'; goto check; }
-elseif($char_2 === ')' && $char_1 === 'a'){ $char_2 = '_'; goto check; }
-elseif($char_2 === '_' && $char_1 === 'a'){ $char_2 = '-'; goto check; }
-elseif($char_2 === '-' && $char_1 === 'a'){ $char_2 = '+'; goto check; }
-elseif($char_2 === '+' && $char_1 === 'a'){ $char_2 = '='; goto check; }
-elseif($char_2 === '=' && $char_1 === 'a'){ $char_2 = '{'; goto check; }
-elseif($char_2 === '{' && $char_1 === 'a'){ $char_2 = '}'; goto check; }
-elseif($char_2 === '}' && $char_1 === 'a'){ $char_2 = '['; goto check; }
-elseif($char_2 === '[' && $char_1 === 'a'){ $char_2 = ']'; goto check; }
-elseif($char_2 === ']' && $char_1 === 'a'){ $char_2 = '|'; goto check; }
-elseif($char_2 === '|' && $char_1 === 'a'){ $char_2 = ';'; goto check; }
-elseif($char_2 === ';' && $char_1 === 'a'){ $char_2 = ':'; goto check; }
-elseif($char_2 === ':' && $char_1 === 'a'){ $char_2 = '<'; goto check; }
-elseif($char_2 === '<' && $char_1 === 'a'){ $char_2 = '>'; goto check; }
-elseif($char_2 === '>' && $char_1 === 'a'){ $char_2 = ','; goto check; }
-elseif($char_2 === ',' && $char_1 === 'a'){ $char_2 = '.'; goto check; }
-elseif($char_2 === '.' && $char_1 === 'a'){ $char_2 = '/'; goto check; }
-elseif($char_2 === '/' && $char_1 === 'a'){ $char_2 = '?'; goto check; }
-elseif($char_2 === '?' && $char_1 === 'a'){ $char_2 = '`'; goto check; }
-elseif($char_2 === '`' && $char_1 === 'a'){ $char_2 = '£'; goto check; }
-elseif($char_2 === '£' && $char_1 === 'a'){ $char_2 = '€'; goto check; }
-elseif($char_2 === '€' && $char_1 === 'a'){ $char_2 = '¢'; goto check; }
-elseif($char_2 === '¢' && $char_1 === 'a'){ $char_2 = '¥'; goto check; }
-elseif($char_2 === '¥' && $char_1 === 'a'){ $char_2 = '§'; goto check; }
-elseif($char_2 === '§' && $char_1 === 'a'){ $char_2 = '0'; goto check; }
-if($char_3 === 'z' && $char_1 === 'a' && $char_2 === 'a'){
-    $char_3 = 'A';
-    goto check;
-}
-elseif($char_3 === 'Z'&& $char_1 === 'a' && $char_2 === 'a'){
-    $char_3 = 0;
-    goto check;
-}
-elseif($char_3 === 'Z' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '!'; goto check; }
-elseif($char_3 === '!' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '~'; goto check; }
-elseif($char_3 === '~' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '@'; goto check; }
-elseif($char_3 === '@' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '#'; goto check; }
-elseif($char_3 === '#' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '$'; goto check; }
-elseif($char_3 === '$' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '%'; goto check; }
-elseif($char_3 === '%' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '^'; goto check; }
-elseif($char_3 === '^' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '&'; goto check; }
-elseif($char_3 === '&' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '*'; goto check; }
-elseif($char_3 === '*' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '('; goto check; }
-elseif($char_3 === '(' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = ')'; goto check; }
-elseif($char_3 === ')' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '_'; goto check; }
-elseif($char_3 === '_' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '-'; goto check; }
-elseif($char_3 === '-' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '+'; goto check; }
-elseif($char_3 === '+' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '='; goto check; }
-elseif($char_3 === '=' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '{'; goto check; }
-elseif($char_3 === '{' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '}'; goto check; }
-elseif($char_3 === '}' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '['; goto check; }
-elseif($char_3 === '[' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = ']'; goto check; }
-elseif($char_3 === ']' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '|'; goto check; }
-elseif($char_3 === '|' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = ';'; goto check; }
-elseif($char_3 === ';' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = ':'; goto check; }
-elseif($char_3 === ':' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '<'; goto check; }
-elseif($char_3 === '<' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '>'; goto check; }
-elseif($char_3 === '>' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = ','; goto check; }
-elseif($char_3 === ',' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '.'; goto check; }
-elseif($char_3 === '.' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '/'; goto check; }
-elseif($char_3 === '/' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '?'; goto check; }
-elseif($char_3 === '?' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '`'; goto check; }
-elseif($char_3 === '`' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '£'; goto check; }
-elseif($char_3 === '£' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '€'; goto check; }
-elseif($char_3 === '€' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '¢'; goto check; }
-elseif($char_3 === '¢' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '¥'; goto check; }
-elseif($char_3 === '¥' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '§'; goto check; }
-elseif($char_3 === '§' && $char_1 === 'a' && $char_2 === 'a'){ $char_3 = '0'; goto check; }
-if($char_4 === 'z' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){
-    $char_4 = 'A';
-    goto check;
-}
-elseif($char_4 === 'Z' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){
-    $char_4 = 0;
-    goto check;
-}
-elseif($char_4 === 'Z' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '!'; goto check; }
-elseif($char_4 === '!' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '~'; goto check; }
-elseif($char_4 === '~' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '@'; goto check; }
-elseif($char_4 === '@' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '#'; goto check; }
-elseif($char_4 === '#' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '$'; goto check; }
-elseif($char_4 === '$' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '%'; goto check; }
-elseif($char_4 === '%' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '^'; goto check; }
-elseif($char_4 === '^' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '&'; goto check; }
-elseif($char_4 === '&' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '*'; goto check; }
-elseif($char_4 === '*' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '('; goto check; }
-elseif($char_4 === '(' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = ')'; goto check; }
-elseif($char_4 === ')' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '_'; goto check; }
-elseif($char_4 === '_' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '-'; goto check; }
-elseif($char_4 === '-' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '+'; goto check; }
-elseif($char_4 === '+' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '='; goto check; }
-elseif($char_4 === '=' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '{'; goto check; }
-elseif($char_4 === '{' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '}'; goto check; }
-elseif($char_4 === '}' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '['; goto check; }
-elseif($char_4 === '[' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = ']'; goto check; }
-elseif($char_4 === ']' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '|'; goto check; }
-elseif($char_4 === '|' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = ';'; goto check; }
-elseif($char_4 === ';' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = ':'; goto check; }
-elseif($char_4 === ':' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_3 = '<'; goto check; }
-elseif($char_4 === '<' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_3 = '>'; goto check; }
-elseif($char_4 === '>' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_3 = ','; goto check; }
-elseif($char_4 === ',' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '.'; goto check; }
-elseif($char_4 === '.' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '/'; goto check; }
-elseif($char_4 === '/' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '?'; goto check; }
-elseif($char_4 === '?' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '`'; goto check; }
-elseif($char_4 === '`' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '£ '; goto check; }
-elseif($char_4 === '£' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '€'; goto check; }
-elseif($char_4 === '€' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '¢'; goto check; }
-elseif($char_4 === '¢' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '¥'; goto check; }
-elseif($char_4 === '¥' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '§'; goto check; }
-elseif($char_4 === '§' && $char_1 === 'a' && $char_2 === 'a' && $char_3 === 'a'){ $char_4 = '0'; goto check; }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Dit scriptje is niet getest, maar volgens mij werkt het wel aardig. Jij zult het af moeten maken  ///
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+$allPossibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+{}|:\"<>?-=[]\;',./'";
 
+$encrypted = "LfIZUJN/1hMeAt2+gSH5qnriYEmONHZPtyvka1uGm7al/aqhdmdnCu6VEXJMhIm/";
 
-
-++$char_1;
-check:
-$pass = $char_1 . $char_2 . $char_3 . $char_4;
-
-start:
-$text = openssl_decrypt('9wJDdUL4YibZQZairPZlj5eEsotjh+LDG78IaZdh4H0=', 'aes128', $pass);
-$text2 = openssl_decrypt('LfIZUJN/1hMeAt2+gSH5qnriYEmONHZPtyvka1uGm7al/aqhdmdnCu6VEXJMhIm/', 'aes128', $pass);
-
-
-if(isset($text) && isset($text2)){
-
-    if(substr($text, -10, 6) === 'ditZitErIn'){
-
-        echo $text;
-        echo $pass;
-    }
-    elseif (substr($text2, -10, 6) === 'ditZit'){
-
-        echo $text;
-        echo $pass;
-    }
-    elseif($pass === '9999'){
-        echo 'no password found';
-    }
-    else{
-        //echo $pass . ' ';
-        goto letters;
+$pass = "";
+$n = 0;
+for($layer0 = 0; $layer0 < strlen($allPossibleChars); $layer0++){
+    for($layer1 = 0; $layer1 < strlen($allPossibleChars); $layer1++){
+        for($layer2 = 0; $layer2 < strlen($allPossibleChars); $layer2++){
+            for($layer3 = 0; $layer3 < strlen($allPossibleChars); $layer3++){
+                $a = $allPossibleChars[0]; //zo haal je de eerste char uit $allPossibleChars
+                $char0 = $allPossibleChars[$layer0];
+                $char1 = $allPossibleChars[$layer1];
+                $char2 = $allPossibleChars[$layer2];
+                $char3 = $allPossibleChars[$layer3];//zo haal je variable met layer0 de uit $allPossibleChars
+                //vanaf hier moet jij beginnen met coden,
+                $pass = $char0 . $char1 . $char2 . $char3;
+                $text = openssl_decrypt($encrypted, 'aes128', $pass);
+                if(substr($text, -10, 10) === 'ditZitErin'){
+                    goto end;
+                }
+            }
+        }
     }
 }
-
-
+end:
+echo $pass . ' ';
+echo $text;
 
 
 
